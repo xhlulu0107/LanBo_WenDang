0b1698723422ad8568762606a698a54279bfd37f
xhlulu0107 <279260811@qq.com>
2020-11-12 16:48:38 +0800

    git config --global user.email "279260811@qq.com" git config --global user.name "xhlulu0107"
    Signed-off-by: xhlulu0107 <279260811@qq.com>

---
+++ src/components/lbdz/Doc/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="question" @click="goto"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'lbdzDoc',
+  data() {
+    return {
+      url: 'http://doc.lbdz.vip/lbdz-vue'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
